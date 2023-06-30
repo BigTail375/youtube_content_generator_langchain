acdc667b1417670345142c61e7ea1b80a914eefd
BigTail375 <shorttail375@gmail.com>
2023-06-30 20:43:25 +0300

    callbacks commit

---
+++ callback.py
@@ -0,0 +1,46 @@
+import asyncio
+from typing import Any, Dict, List
+
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import LLMResult, HumanMessage
+from langchain.callbacks.base import AsyncCallbackHandler, BaseCallbackHandler
+
+
+class MyCustomSyncHandler(BaseCallbackHandler):
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        print(f"Sync handler being called in a `thread_pool_executor`: token: {token}")
+
+
+class MyCustomAsyncHandler(AsyncCallbackHandler):
+    """Async callback handler that can be used to handle callbacks from langchain."""
+
+    async def on_llm_start(
+        self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
+    ) -> None:
+        """Run when chain starts running."""
+        print("zzzz....")
+        await asyncio.sleep(0.3)
+        class_name = serialized["name"]
+        print("Hi! I just woke up. Your llm is starting")
+
+    async def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
+        """Run when chain ends running."""
+        print("zzzz....")
+        await asyncio.sleep(0.3)
+        print("Hi! I just woke up. Your llm is ending")
+
+chat = ChatOpenAI(
+    max_tokens=25,
+    streaming=True,
+    callbacks=[MyCustomSyncHandler(), MyCustomAsyncHandler()],
+)
+
+async def generate():
+    result = await chat.agenerate([[HumanMessage(content="Tell me a joke")]])
+    return result
+
+# Call the asynchronous function
+
+if __name__ == '__main__':
+    asyncio.run(generate())
+
